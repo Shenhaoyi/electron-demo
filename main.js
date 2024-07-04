648a37b5afd112e86d25518ec32e8920c6c4cbbf
Shenhaoyi <shenhaoyi04@163.com>
2024-07-04 12:43:16 +0800

    manage window's lifecycle

--- main.js
+++ main.js
@@ -11,4 +11,15 @@ const createWindow = () => {
 
 app.whenReady().then(() => {
   createWindow();
+
+  // macOS 平台，当单击dock图标时，如果没有窗口则重新创建窗口
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+// macOS 以外的平台，当所有窗口关闭时，退出应用
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
 });
