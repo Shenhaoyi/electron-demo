da877b46b26e10b1219881d5bf097038efcb5014
Shenhaoyi <shenhaoyi04@163.com>
2024-07-05 11:43:22 +0800

    preload script

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
 const { app, BrowserWindow } = require('electron');
+const path = require('node:path');
 
 const createWindow = () => {
   const win = new BrowserWindow({
     width: 800,
     height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'), // preload 的资源
+    },
   });
 
   win.loadFile('index.html');
